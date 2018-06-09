d5efd62495e9e55f9bf041b7f1d53059ab2a0651
admin <admin@MacBook-Pro-de-admin.local>
2018-06-09 19:36:22 -0300

    Added an icon to all pages

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Lucas Joshua Pires</title>
         <meta charset="utf-8">
+        <link rel="icon" href="icon/equinoxe_64.png">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="css/styler.css">
         <link rel="stylesheet" href="css/gallery.css">
