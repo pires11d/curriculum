d30a83da885cc297019da7c559f11fcf156ceb84
admin <pires11d@hotmail.com>
2018-06-12 15:49:12 -0300

    Added chemengbrasil Icon

--- projects3.php
+++ projects3.php
@@ -12,11 +12,26 @@
         <p></p>
         <!--HEADER 1-->
         <div id="c1_1" class="container">
-            <a href="https://chemengbrasil.wixsite.com/index"><h1>ChemEng Brasil</h1></a>
+            <h1><img src="icon/chemengbrasil.png"><a href="https://chemengbrasil.wixsite.com/index">  ChemEng Brasil</h1></a>
             <p>Website educativo, sem fins lucrativos, dedicado a compartilhar conhecimentos de Engenharia Química, Engenharia de Alimentos, Farmácia, Botânica e Agronomia.</p>
         </div>
         <p></p>
         <!-- HEADER 2 -->
+        <div id="c1_3" class="container">
+            <h1><img src="icon/python.png"><a href="https://github.com/pires11d/pythonClasses">  pythonClasses Repository</a></h1>
+        </div>
+        <h6></h6>
+        <!-- HEADER 3 -->
+        <div id="c1_4" class="container">
+            <h1><img src="icon/excel.png"><a href="https://github.com/pires11d/xlSheets">  xlSheets Repository</a></h1>
+        </div>
+        <h6></h6>
+        <!-- HEADER 4 -->
+        <div id="c1_5" class="container">
+            <h1><img src="icon/matlab.png"><a href="https://github.com/pires11d/matlabScripts">  matlabScripts Repository</a></h1>
+        </div>
+        <p></p>
+        <!-- HEADER 5 -->
         <div id="c1_2" class="container">
             <h1>Counter-Strike:Source Maps</h1>
             <div id="hidden2">
@@ -28,21 +43,6 @@
                     <li><a class="link" href="https://gamebanana.com/maps/163683">Surf_Tron_NJV</a></li>
                 </h2></ul>
             </div>
-        </div>
-        <p></p>
-        <!-- HEADER 3 -->
-        <div id="c1_3" class="container">
-            <h1><img src="icon/python.png"><a href="https://github.com/pires11d/pythonClasses">  pythonClasses Repository</a></h1>
-        </div>
-        <h6></h6>
-        <!-- HEADER 4 -->
-        <div id="c1_4" class="container">
-            <h1><img src="icon/excel.png"><a href="https://github.com/pires11d/xlSheets">  xlSheets Repository</a></h1>
-        </div>
-        <h6></h6>
-        <!-- HEADER 5 -->
-        <div id="c1_5" class="container">
-            <h1><img src="icon/matlab.png"><a href="https://github.com/pires11d/matlabScripts">  matlabScripts Repository</a></h1>
         </div>
     </body>
     <script src="js/jquery.js"></script>
