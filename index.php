6b1d2839d3b38fc55a0cdb7da07996783aecf633
Pira <pires11d@hotmail.com>
2020-02-26 15:51:02 -0300

    Added Trevo as a job

--- index.php
+++ index.php
@@ -30,54 +30,48 @@
                 <span class="dot" onclick="currentSlide(5)"></span>
                 <span class="dot" onclick="currentSlide(6)"></span>
                 <span class="dot" onclick="currentSlide(7)"></span>
-                <span class="dot" onclick="currentSlide(8)"></span>
             </div>
             <!-- Initial Slide -->
             <div id="stdSlide">
-                <div class="numbertext">1 / 8</div>
+                <div class="numbertext">1 / 7</div>
                 <img class="screens" src="img/ExtraOil.gif">
-                <div id="caption" class="text">Interface para o programa desenvolvido na Tubanharon Process Engineering</div>
+                <div id="caption" class="text">Interface para o simulador de extração de soja, desenvolvido na Tubanharon Process Engineering</div>
             </div>
             <!-- Other Slides -->
             <div class="mySlides">
-                <div class="numbertext">1 / 8</div>
+                <div class="numbertext">1 / 7</div>
                 <img class="screens" src="img/ExtraOil.gif">
-                <div class="text">Interface para o programa desenvolvido na Tubanharon Process Engineering</div>
+                <div class="text">Interface para o simulador de extração de soja, desenvolvido na Tubanharon Process Engineering</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">2 / 8</div>
+                <div class="numbertext">2 / 7</div>
                 <img class="screens" src="img/A2Rsheet.PNG">
-                <div class="text">Interface principal - Planilha desenvolvida na Tubanharon Process Engineering</div>
+                <div class="text">Interface principal da planilha de cálculos para uma Unidade de Regeneração de Ácidos (U.R.A)</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">3 / 8</div>
+                <div class="numbertext">3 / 7</div>
                 <img class="screens" src="img/A2Rsheet1.PNG">
                 <div class="text">Input de dados - Planilha desenvolvida na Tubanharon Process Engineering</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">4 / 8</div>
+                <div class="numbertext">4 / 7</div>
                 <img class="screens" src="img/TEA.PNG">
-                <div class="text">Análise Técnico Econômica para processos de Crushing</div>
+                <div class="text">Planilha de Análise Técnico-Econômica para processos de Crushing</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">5 / 8</div>
+                <div class="numbertext">5 / 7</div>
                 <img class="screens" src="img/PlantEconomics.PNG">
-                <div class="text">"PlantEconomics" - Planilha genérica para cálculo de custos de equipamentos e viabilidade econômica de projetos</div>
+                <div class="text">Planilha genérica para cálculo de custos de equipamentos e viabilidade econômica de projetos</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">6 / 8</div>
+                <div class="numbertext">6 / 7</div>
                 <img class="screens" src="img/PlantDesign.PNG">
-                <div class="text">"PlantDesign" - Planilha genérica para projetos de engenharia química</div>
+                <div class="text">Planilha genérica para projetos de engenharia química</div>
             </div>
             <div class="mySlides">
-                <div class="numbertext">7 / 8</div>
-                <img class="screens" src="img/OxyFree.PNG">
-                <div class="text">Interface principal - Simulador de Decapagem Ácida em Excel</div>
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">8 / 8</div>
+                <div class="numbertext">7 / 7</div>
                 <img class="screens" src="img/OxyFree1.PNG">
-                <div class="text">Saída gráfica - Simulador de Decapagem Ácida em Excel</div>
+                <div class="text">Saída gráfica - Simulador de Decapagem Ácida feito em Excel + Octave</div>
             </div>
         </div>
     </div>
--- projects2.php
+++ projects2.php
@@ -17,7 +17,7 @@
             <h1><img src="icon/anjo.png" width="48px"> Anjo Tintas</h1>
             <div id="hidden1">
                 <h3>Nov 2016 – Mar 2017</h3>
-                <ul>
+                <ul style="list-style: none;">
                     <li><h2>Simulador de Evaporação</h2></li>
                 </ul>
                 <h4>Assistência no desenvolvimento de um software capaz de simular o comportamento de misturas não-ideais contendo diferentes solventes e resinas, bem como em sua documentação técnica.</h4>
@@ -31,44 +31,56 @@
             <div id="hidden2">
                 <h3>Mar 2017 – Ago 2017</h3>
                 <h4>Estágio obrigatório na empresa Tubanharon, visando a consultoria e o desenvolvimento de softwares para as indústrias de processos.</h4>
+                <h4>Atividades desenvolvidas o estágio na empresa:</h4>
                 <ul style="list-style: none;">
-                    <div class="container">
-                        <li>
-                        <h4>Planilha dinâmica com cálculos em VBA que resolvem balanços de massa e energia (a parâmetros concentrados) para um sistema de decapagem ácida genérico com “n” tanques. Calcula a taxa de decapagem para bobinas de aço, bem como a concentração e temperatura dos banhos. O modelo transiente foi baseado em equações diferenciais ordinárias de 1ª ordem descrito no artigo de Noh (2015). A abordagem computacional para resolver as equações foi a comunicação entre os softwares Excel e Octave.</h4>
-                        </li>
-                    </div>
-                    <h6></h6>
-                    <div class="container">
-                        <li><h2>-</h2></li>
-                        <h4>Planilha com cálculos em VBA que foi criada com base em uma planta de Unidade de Regeneração de Ácidos (U.R.A.), a qual recupera o ácido utilizado em plantas de decapagem de chapas de aço, e teve como objetivo realizar interativamente o fechamento do balanço de massa de todas as correntes do processo, através da entrada de dados coletados em certos pontos de operação da planta.</h4>
-                    </div>
-                    <h6></h6>
-                    <div class="container">
-                        <li><h2>PlantEconomics</h2></li>
+                    <li>
+                        <li><h2>Simulador de Decapagem Ácida</h2></li>
+                        <h4>Desenvolvimento de planilha dinâmica (com VBA) que resolve balanços de massa e energia (a parâmetros concentrados) para um sistema de decapagem ácida genérico com “n” tanques. Calcula a taxa de decapagem para bobinas de aço, bem como a concentração e temperatura dos banhos. O modelo transiente foi baseado em equações diferenciais ordinárias de 1ª ordem descrito no artigo de Noh (2015). A abordagem computacional para resolver as equações foi a comunicação entre os softwares Excel e Octave.</h4>
+                    </li>
+                    <li>
+                        <li><h2>Planilha de cálculo para uma Unidade de Regeneração de Ácidos</h2></li>
+                        <h4>Desenvolvimento de planilha (com VBA e UserForms) para estimar as correntes de massa e de energia faltantes de uma planta de Unidade de Regeneração de Ácidos (U.R.A.). Teve como objetivo realizar interativamente o fechamento do balanço de massa de todas as correntes do processo, através da entrada de dados coletados em certos pontos de operação da planta.</h4>
+                    </li>
+                    <li>
+                        <li><h2>Planilha para cálculo de viabilidade técnico-econômica</h2></li>
                         <h4>Planilha dinâmica para análise técnico-econômica de projetos industriais. A interface principal da planilha apresenta o gráfico do Valor Presente Líquido com o tempo, bem como os principais indicadores de viabilidade de um projeto: Taxa Interna de Retorno (TIR), Índice de Lucratividade (IL), e Valor Presente Líquido (VPL).</h4>
-                    </div>
+                    </li>
                 </ul>
                 <p></p>
                 <h3>Ago 2017 – Dez 2019</h3>
                 <h4>Contratado pela empresa Tubanharon, como Analista Técnico e Desenvolvedor de Software.</h4>
+                <h4>Durante este período, foram desenvolvidos os seguintes projetos, e realizada a manutenção dos mesmos:</h4>
                 <ul style="list-style: none;">
-                    <div class="container">
-                        <li><h2>-</h2></li>
-                        <h4>Desenvolvimento de um simulador (em VB.NET) de extração de óleo de soja para extratores do tipo Rotocel.</b> Melhoria da interface gráfica, bem como contribuições na modelagem matemática e consequente implementação computacional. Gestão de conteúdo e compilação da documentação técnica.</h4>
-                    </div>
-                    <h6></h6>
-                    <div class="container">
-                        <li><h2>-</h2></li>
-                        <h4>(<i>Technical-Economical Analysis</i>): Planilha com VBA que calcula e compara diferentes simulações feitas pelo <i>ExtraOil.M2</i>; realiza balanços de massa e de energia, e fornece os índices econômicos em uma unidade industrial de Crushing de óleo de soja.</b></h4>
-                    </div>
-                    <h6></h6>
-                    <div class="container">
-                        <li><h2>-</h2></li>
-                        <h4>Planilha com VBA que calcula os balanços de massa e de energia, bem como a viabilidade econômica de um projeto conceitual de uma unidade de processamento de <i>Aloe vera</i>. Posteriormente é transformada na planilha genérica Plant.Design, que serviria para qualquer processo industrial.</h4>
-                    </div>
+                    <li>
+                        <li><h2>Simulador de Extração</h2></li>
+                        <h4>Desenvolvimento de um simulador (em VB.NET) de extração de óleo de soja para extratores do tipo Rotocel.</b> Melhoria da interface gráfica, bem como contribuições na modelagem matemática e consequente implementação computacional. Gestão de conteúdo e compilação da documentação técnica. Criação de uma nova interface gráfica para o usuário em Windows Forms, com o objetivo de melhorar a performance do programa, bem como remover as dependências de ferramentas como MS-Excel.</h4>
+                    </li>
+                    <li>
+                        <li><h2>Programa para cálculos hidráulicos</h2></li>
+                        <h4>Manutenção e integração do programa previamente desenvolvido na empresa, o qual foi feito para realizar o cálculo de balanços de massa e energia mecânica para qualquer sistema de tubulações, com "n" acessórios e/ou bombas. Feito inteiramente em Python, não possui interface gráfica, apenas no estilo CLI (<i>command line interface</i>)</h4>
+                    </li>
+                    <li>
+                        <li><h2>Planilha de cálculos para simulação do <i>Crushing</i> de soja</h2></li>
+                        <h4>Realiza cálculos de engenharia para cada seção de processo de uma unidade de <i>Crushing</i>, desde a preparação da massa até a destilação e recuperação de solvente; realiza balanços de massa e de energia, e fornece os índices econômicos em uma unidade industrial de Crushing de óleo de soja.</h4>
+                    </li>
+                    <li>
+                        <li><h2>Planilha auxiliar para projeto de uma Unidade de Processamento da Aloe vera</h2></li>
+                        <h4>Planilha com VBA que calcula os balanços de massa e de energia, bem como a viabilidade econômica de um projeto conceitual de uma unidade de processamento de <i>Aloe vera</i>.</h4>
+                    </li>
                 </ul>
                 <p></p>
                 <h4><b>Local: </b>km 01 4o. Andar, Sala 3.02, 401, R. Parque Tecnológico Alfa - João Paulo, Florianópolis, SC, Brasil.</h4>
+            </div>
+        </div>
+        <p></p>
+        <!--HEADER 3-->
+        <div id="c1_3" class="container">
+            <h1><img src="icon/trevo.png" width="72px">Trevo Importadora LTDA</h1>
+            <div id="hidden3">
+                <h3>Dez 2019 – Presente</h3>
+                <h4>Consultoria técnica e desenvolvimento de ferramenta computacional (em VBA) visando a melhoria do controle de estoque para os produtos da empresa.</h4>
+                <p></p>
+                <h4><b>Local: </b>Alcides Bacin, 348, Área Industrial 2, Bairro São Paulo, Lages, SC, Brasil.</h4>
             </div>
         </div>
         <p></p>
